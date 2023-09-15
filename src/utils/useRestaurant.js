64d1925c2a45d3b8c5414cf1114ce1f75e922def
Prathamesh47 <pratham03d@gmail.com>
2023-09-15 11:01:32 +0530

    Added Async FUnc

--- src/utils/useRestaurant.js
+++ src/utils/useRestaurant.js
@@ -5,18 +5,19 @@ const useRestaurant = (resId) => {
     const [restaurant,setRestaurant] = useState(null);
     
     useEffect(()=>{
+        const getRestaurantInfo = async  ()=>{
+        
+            const response =await fetch(FETCH_MENU_URL+resId);
+            const res_data=await response.json();
+            // console.log(json);
+            setRestaurant((res_data?.data?.cards[0]?.card?.card?.info));
+            console.log((res_data?.data?.cards[0]?.card?.card?.info));
+        
+        }
         getRestaurantInfo();
     },[]);
 
-    async function getRestaurantInfo(){
-        
-        const response =await fetch(FETCH_MENU_URL+resId);
-        const res_data=await response.json();
-        // console.log(json);
-        setRestaurant((res_data?.data?.cards[0]?.card?.card?.info));
-        console.log((res_data?.data?.cards[0]?.card?.card?.info));
-    
-    }
+   
         return restaurant
     
 };
